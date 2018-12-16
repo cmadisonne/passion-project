11a384e05d1cb677423e147b7c6b4fb7cf040904
Lotus <cmadisonne@gmail.com>
2018-12-16 14:06:41 -0600

    InProgress

--- Yoga/YogaApp/admin.py
+++ Yoga/YogaApp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import SignUp
+from .models import Contact, Events
 
-admin.site.register(SignUp)
+admin.site.register(Contact)
+admin.site.register(Events)
--- Yoga/YogaApp/models.py
+++ Yoga/YogaApp/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class SignUp(models.Model):
+class Contact(models.Model):
     name = models.CharField(max_length=50)
     number = models.CharField(max_length=12)
     message = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
+
+class Events(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    time = models.TimeField(auto_now=False, auto_now_add=False)
+    description = models.CharField(max_length=500)
+    image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
